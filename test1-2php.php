98167a91c4ddf0cf0e0a33398366b59323d058db
javiermart18 <45857679+javiermart18@users.noreply.github.com>
2019-01-10 19:12:56 +0100

    Add files via upload

--- test1-2php.php
+++ test1-2php.php
@@ -42,17 +42,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
 ?>
 
-<form action="test1-1php.php" method="post">
+<form action="test1-2php.php" method="post">
     
-    <h1>Test 1.1</h1>
+    <h1>Test 1.2</h1>
 
 <?php
     
- echo "<strong>1- PHP is a ...</strong>";
+ echo "<strong>1- Which of the following method of Exception class returns source filename?</strong>";
  echo "&nbsp;&nbsp;&nbsp;<font color='red'>&nbsp$radioErr</font>";
  
- $answer1=["A) Server-side programming language","B) Website",
-          "C) Marking language","D) Homepage"];
+ $answer1=["A) getMessage()",
+           "B) getCode()",
+          "C) getFile()",
+           "D) getLine()"];
 
 foreach( $answer1 as $answer1s)
 {	   
@@ -60,43 +62,40 @@ foreach( $answer1 as $answer1s)
 };
 
 
-echo "<strong>2- In PHP, $ language['php'] is an example of</strong>";  echo "<font color='red'>&nbsp;&nbsp;&nbsp;$radio1Err</font>"; 
+echo "<strong>2- Which of the following magic constant of PHP returns current line number of the file?</strong>";  echo "<font color='red'>&nbsp;&nbsp;&nbsp;$radio1Err</font>"; 
 
- $answer2=["A) Associative Array","B) Multidimensional array",
-          "C) Superglobal array","D) The previous answers are not correct "];
+ $answer2=["A) _LINE_","B) _FILE_",
+          "C) _FUNCTION_","D) _CLASS_"];
 
 foreach( $answer2 as $answer2s)
 {	   
 		echo '<p><input type="radio" name="radio1" value="'.$answer2s.'"> '.$answer2s.'</p>';
 };
 
-echo "<strong>3- In PHP, what is the next code for? if (isset ($ variable))</strong>";  echo "<font color='red'>&nbsp;&nbsp;&nbsp;$radio2Err</font>"; 
+echo "<strong>3- Which of the following array represents an array with a numeric index?</strong>";  echo "<font color='red'>&nbsp;&nbsp;&nbsp;$radio2Err</font>"; 
 
- $answer3=["A) Check if $ variable is defined and has a non-zero value",
-           "B) Check if $ variable is a variable of scalar type",
-          "C) Check if $ variable is a variable of vector type (array)",
-           "D) The previous answers are not correct"];
+ $answer3=["A) Numeric Array",
+           "B) Associative Array",
+          "C) Multidimentional Array",
+           "D) None of the above"];
 
 foreach( $answer3 as $answer3s)
 {	   
 		echo '<p><input type="radio" name="radio2" value="'.$answer3s.'"> '.$answer3s.'</p>';
 };
 
-echo "<strong>4- What does PHP mean?</strong>";  echo "<font color='red'>&nbsp;&nbsp;&nbsp;$radio3Err</font>"; 
+echo "<strong>4- If there is any problem in loading a file then the include() function generates a warning but the script will continue execution.</strong>";  echo "<font color='red'>&nbsp;&nbsp;&nbsp;$radio3Err</font>"; 
 
- $answer4=["A) Private Home Page",
-           "B) PHP: Hypertext Preprocessor",
-          "C) Personal Hypertext Processor",
-           "D) Personal Home Page"];
+ $answer4=["A) True","B) False"];
 
 foreach( $answer4 as $answer4s)
 {	   
 		echo '<p><input type="radio" name="radio3" value="'.$answer4s.'"> '.$answer4s.'</p>';
 };
 
-echo "<strong>5- In PHP, all the variables start with the symbol</strong>";  echo "<font color='red'>&nbsp;&nbsp;&nbsp;$radio4Err</font>"; 
+echo "<strong>5- Which of the following function is used to get the size of a file?</strong>";  echo "<font color='red'>&nbsp;&nbsp;&nbsp;$radio4Err</font>"; 
 
- $answer5=["A) !","B) &","C) $","D) #"];
+ $answer5=["A) fopen()","B) fread()","C) filesize()","D) file_exist()"];
 
 foreach( $answer5 as $answer5s)
 {	   
@@ -113,11 +112,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
 $totalCorrect = 0;
 
-if ($result1 =="A) Server-side programming language") {$totalCorrect++;}
-if ($result2 =="A) Associative Array") {$totalCorrect++;}
-if ($result3 =="A) Check if $ variable is defined and has a non-zero value") {$totalCorrect++;}
-if ($result4 =="B) PHP: Hypertext Preprocessor") {$totalCorrect++;}
-if ($result5 =="C) $") { $totalCorrect++;}
+if ($result1 == "C) getFile()") {$totalCorrect++;}
+if ($result2 == "A) _LINE_") {$totalCorrect++;}
+if ($result3 == "A) Numeric Array") {$totalCorrect++;}
+if ($result4 == "A) True") {$totalCorrect++;}
+if ($result5 == "C) filesize()") { $totalCorrect++;}
 
 echo "<div id='results'><h1>Your score is  $totalCorrect / 5 </h1></div>";
     
