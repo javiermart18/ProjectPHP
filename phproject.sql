1e4f1b6dc3f8bd1015bd7e882a665ab61a96886a
csalazar98 <34164596+csalazar98@users.noreply.github.com>
2019-01-21 12:46:04 +0100

    Update phproject.sql

--- phproject.sql
+++ phproject.sql
@@ -2,14 +2,26 @@ CREATE DATABASE phproject;
 
 USE phproject;
 
-CREATE TABLE users (
-user_id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
-username VARCHAR(20) NOT NULL,
-pass CHAR(128) NOT NULL,
-email VARCHAR(60) NOT NULL,
-registration_date DATETIME NOT NULL,
-clashes INT NOT NULL,
-points_clashes INT NOT NULL,
-total_points INT NOT NULL,
-PRIMARY KEY (user_id)
-);
+CREATE TABLE users(
+user_id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT ,
+username VARCHAR( 20 ) NOT NULL ,
+passCHAR( 128 ) NOT NULL ,
+email VARCHAR( 60 ) NOT NULL ,
+registration_date DATETIME NOT NULL ,
+total_points INT,
+PRIMARY KEY ( user_id )
+)
+
+create table quiz (
+id_quiz MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+id_user int,
+name varchar(100),
+score int,
+date DATETIME NOT NULL,
+PRIMARY KEY (id_quiz)
+)
+
+/*
+Relationships have been made from phpMyAdmin, indicating that table users is related to the quiz table where the referenced key is user_id from the users table and the foreign key is id_user from the quiz table. 
+*/
+
